2d7f5de1aab2e74d222565610d49fa8be62eec49
Shole <navatejapendiala@Shole.local>
2024-05-02 01:00:06 +0530

    ren

--- Components/AuthComponents.js
+++ Components/AuthComponents.js
@@ -120,7 +120,7 @@ const login = async (req, res) => {
       maxAge: 15 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "None",
     });
 
     temp = {
